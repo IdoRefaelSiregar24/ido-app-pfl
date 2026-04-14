b0b15dd3f780dce49c9319f8d1325b0f4655e06e
Ido <idosiregar2006@gmail.com>
2026-04-14 17:17:12 +0700

    Membuat Sebauh Halaman dengan Hasil Implementasi dari Desaign Figma

--- src/pertemuan-5/main.jsx
+++ src/pertemuan-5/main.jsx
@@ -1,9 +1,25 @@
 import { createRoot } from "react-dom/client";
-import TailwindCSS from "./TailwindCSS";
+
 import "./assets/tailwind.css";
+import Dashboard from "./pages/Dashboard";
+import Sidebar from "./layouts/Sidebar";
+import Header from "./layouts/Header";
 
 createRoot(document.getElementById("root")).render(
-  <div>
-    <p>Test</p>
+  <div id="app-container" className="bg-gray-100 h-screen flex w-full">
+
+    <div id="layout-wrapper" className="flex flex-row flex-1 w-full overflow-hidden">
+      <Sidebar />
+      
+      <div id="main-content" className="flex-1 p-4 flex flex-col overflow-y-auto">
+        <Header />
+        
+        <div id="dashboard-wrapper" className="mt-4">
+          <Dashboard />
+        </div>
+      </div>
+      
+    </div>
+
   </div>
 );
